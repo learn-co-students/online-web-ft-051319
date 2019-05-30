7fcc253af622da33817b99270fd97c842120bb10
critsmet <chrismetzgr@gmail.com>
2019-05-30 18:12:35 -0400

    added nokogiri discussion

---
+++ 05-scraping-w-nokogiri/scraping-w-nokogiri.rb
@@ -0,0 +1,40 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class Alumni
+
+  @@all = []
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
+
+base_url = "https://flatironschool.com/alumni/"
+
+
+html = open(base_url)
+
+parsed_html = Nokogiri::HTML(html)
+
+alumni_elements = parsed_html.css(".text-block.util__margin-bm")
+
+alumni_elements.each do |element|
+  puts element.text
+end
+
+# Alumni.all
+
+#https://www.w3schools.com/cssref/css_selectors.asp
+#two ways to grab elements by their css
+# tag selector => div, h2, h1, p, span
+# class selector uses the '.' => .yk4f6w-0 .gRWfND
