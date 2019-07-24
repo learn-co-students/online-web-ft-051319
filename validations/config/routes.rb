fe74904f227e26f7c44f14a3a0f250ca59611205
critsmet <chrismetzgr@gmail.com>
2019-07-24 08:00:25 -0400

    rails validations

--- validations/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students
-  # get '/students', to: 'students#index'
-  # get '/students/:id', to: 'students#show'
-  # does the same as resources :students, only: [:index]
-end
