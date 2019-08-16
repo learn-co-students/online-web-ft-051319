e600b9b5c26d7bae1f5e78e2051ee386e83e7da6
critsmet <chrismetzgr@gmail.com>
2019-08-16 17:03:21 -0400

    Intro to Events

---
+++ 23-javascript-events/index.js
@@ -0,0 +1,34 @@
+//First step: grab the elements that we want to add event listeners to
+let addEmojiArea = document.getElementById("add-emoji-area")
+let addButton = document.getElementById("add-button")
+let emojiBank = document.getElementById("emoji-bank")
+// console.log("AddEmojiArea", addEmojiArea)
+// console.log("Add Button", addButton)
+console.log("Emoji Bank", emojiBank)
+
+addEmojiArea.addEventListener('click', handleAddEmojiAreaClick)
+
+let form = `
+  <form id="emoji-form">
+    <input id="input" type="text">
+    <input id="submit-emoji" type="submit">
+  </form>
+`
+
+function handleAddEmojiAreaClick(e){
+  console.log(e.target);
+  if (e.target.id == "add-button"){
+    addEmojiArea.innerHTML = form
+    // let emojiForm = document.getElementById("emoji-form")
+    // emojiForm.addEventListener('click', function)
+    // DON'T DO THIS! HANDLE THE EVENTS USING THE EVENT LISTENER ON THE PARENT DIV
+  }
+  if (e.target.id == "submit-emoji"){
+    e.preventDefault()
+    let inputField = document.getElementById("input")
+    let inputEmoji = inputField.value
+    emojiBank.innerHTML += `<li>${inputEmoji}</li>`
+    addEmojiArea.innerHTML = addButton
+  }
+
+}
---
+++ 23-javascript-events/readme.md
@@ -0,0 +1,21 @@
+# Key Questions
+* What is an event in JavaScript?
+  * What kinds of events are there in the DOM?
+     * https://codepen.io/telegram/pen/PJpPGm
+   * What is event bubbling and why is it important to know about?
+     * https://javascript.info/bubbling-and-capturing
+* How do I make some code run when an event occurs in my DOM?
+* What is a callback?
+* What is the difference between function definition (reference) and function invocation?
+* How can I listen for events on a DOM node / HTML element?
+* What calls the callback that I pass to addEventListener? What gets passed into that callback?
+
+https://www.w3schools.com/jsref/dom_obj_event.asp
+
+# Deliverables
+As a developer I want...
+
+* a form to appear in place of the 'Add Emoji' button when the 'Add Emoji' button is clicked
+* an alert saying "Emoji Submitted!" whenever an emoji is submitted
+* an emoji I click on turns into '🤪'
+* changing the background color of something at some point 
